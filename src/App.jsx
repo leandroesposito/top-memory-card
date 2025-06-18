7a2adfbd21145af3f34bb1e681a1d80470d47418
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-06-18 18:18:04 -0300

    Extract form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState, useRef } from "react";
 import "./App.css";
+import Form from "./components/Form.jsx";
 
 const gameSizes = [4, 6, 8, 10, 12, 14];
 const pageSize = gameSizes[gameSizes.length - 1];
@@ -56,31 +57,11 @@ function App() {
 
   return (
     <>
-      <form>
-        <fieldset>
-          <legend>Select number of tiles for the game.</legend>
-          {gameSizes.map((gs) => (
-            <div id="form-row" key={gs}>
-              <input
-                type="radio"
-                value={gs}
-                name="game-size"
-                id={`game-size-${gs}`}
-              />
-              <label htmlFor={`game-size-${gs}`}>{gs}</label>
-            </div>
-          ))}
-        </fieldset>
-        {gameIsReady ? (
-          <button type="button" onClick={handleSubmit}>
-            Play!
-          </button>
-        ) : (
-          <button type="button" disabled={true}>
-            Loading data...
-          </button>
-        )}
-      </form>
+      <Form
+        gameSizes={gameSizes}
+        gameIsReady={gameIsReady}
+        handleSubmit={handleSubmit}
+      />
     </>
   );
 }
---
+++ src/components/Form.jsx
@@ -0,0 +1,29 @@
+export default function Form({ gameSizes, gameIsReady, handleSubmit }) {
+  return (
+    <form>
+      <fieldset>
+        <legend>Select number of tiles for the game.</legend>
+        {gameSizes.map((gs) => (
+          <div id="form-row" key={gs}>
+            <input
+              type="radio"
+              value={gs}
+              name="game-size"
+              id={`game-size-${gs}`}
+            />
+            <label htmlFor={`game-size-${gs}`}>{gs}</label>
+          </div>
+        ))}
+      </fieldset>
+      {gameIsReady ? (
+        <button type="button" onClick={handleSubmit}>
+          Play!
+        </button>
+      ) : (
+        <button type="button" disabled={true}>
+          Loading data...
+        </button>
+      )}
+    </form>
+  );
+}
